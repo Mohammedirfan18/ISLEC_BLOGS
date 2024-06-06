a579f779f5aa5f60f24610e879ce105a70cf174a
mohammed irfan <irfancodes1@gmail.com>
2024-06-06 22:02:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import {login,logout} from './store/authSlice'
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import { Outlet } from 'react-router-dom';
+function App() {
+   const [loading,setLoading] = useState(true);
+   const dispatch=useDispatch();
+
+   useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData)
+        {
+          dispatch(login({userData}))
+        }else{
+          dispatch(logout())
+        }
+    })
+    .catch((error)=>{
+      console.log("Error fetching user data:", error);
+    })
+    .finally(()=>setLoading(false))
+   },[])
+
+  return !loading ? (
+    <div className='min-h-screen bg-gray-400 flex flex-wrap content-between '>
+      <div className='w-full-block'>
+        <Header/>
+   <Outlet/>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
+}
+
+export default App
